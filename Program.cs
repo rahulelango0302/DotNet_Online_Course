e024fbe1ecff54419ba117bb1919a6e3c245431b
Raghul <admin@Raghuls-Mac-mini.local>
2025-01-05 12:22:37 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// We have 2 parts here, one is service configuration and another is middleware configuration
+// Service configuration is where we add services to the container using dependency injection
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Wherever app starts it means it's middleware configuration
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
