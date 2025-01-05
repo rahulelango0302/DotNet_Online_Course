d47a7a5d3d0f47b5c57029c911cc0f2785b4ae64
rahulelango0302 <rahulelango1999@gmail.com>
2025-01-05 12:30:33 +0530

    Initial commit 2

--- obj/Debug/net8.0/LSC.OnlineCourse.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/LSC.OnlineCourse.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LSC.OnlineCourse.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e024fbe1ecff54419ba117bb1919a6e3c245431b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LSC.OnlineCourse.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LSC.OnlineCourse.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
